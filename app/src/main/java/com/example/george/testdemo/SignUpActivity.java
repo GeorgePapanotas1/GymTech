2452e8855d1cd764d56d3a668ded70ed1b826634
GeorgePapanotas1 <georgepapanotas1@gmail.com>
2018-03-07 17:30:24 +0200

    Create

---
+++ app/src/main/java/com/example/george/testdemo/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.george.testdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by George on 07-Mar-18.
+ */
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_form);
+    }
+}
